419a548a6c31a347cbe8e79026c7fb13a45d6392
Arkadii Yakovets <ark@cho.red>
2024-03-07 08:59:01 -0800

    Add Python 3.12 support
      - Use Python 3.8-3.12 for test matrix
      - Bump GH action versions
      - Fix configparser SafeConfigParser name issue (the initial one)
      - Replace pkg_resources with importlib.metadata
      - Fix failing test

--- src/lingva/extractors/__init__.py
+++ src/lingva/extractors/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
-from pkg_resources import DistributionNotFound
-from pkg_resources import working_set
+
+from importlib.metadata import entry_points
+
 import abc
 import collections
 import os
@@ -162,13 +163,8 @@ class Extractor(object):
 
 
 def register_extractors():
-    for entry_point in working_set.iter_entry_points("lingva.extractors"):
-        try:
-            extractor = entry_point.load(require=True)
-        except DistributionNotFound:
-            # skip this entry point since at least one required dependency can
-            # not be found
-            extractor = None
+    for entry_point in entry_points("lingua.extractors"):
+        extractor = entry_point.load()
         if extractor:
             if not issubclass(extractor, Extractor):
                 raise ValueError("Registered extractor must derive from ``Extractor``")
